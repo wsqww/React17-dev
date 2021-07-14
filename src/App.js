889fef9a2854f3706344934339c0de9e8ac866fd
wsq <wangsq0926@163.com>
2021-07-14 14:29:22 +0800

    react-router  test

--- src/App.js
+++ src/App.js
@@ -1,98 +1,32 @@
 import React from "react";
 import "./App.css";
 
+import { BrowserRouter as Router, Route, Link, Switch, Redirect } from 'react-router-dom';
+
+import JsxTest from "./pages/Jsx/jsx";
+
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      number: 0,
-      text: "hello",
-      numArr: [100, 200, 300, 400, 500],
-      inputValue: "input",
-      textareaValue: "textarea"
-    };
-  }
-
-  updateText() {
-    this.setState({
-      number: this.state.number + 1,
-      text: "hello word"
-    });
-  }
-
   render() {
-    return <div className="App">{this.dom()}</div>;
-  }
-
-  dom() {
-    return (
-      <div>
-        {/* 注释 */}
-        {/* 动态数据 */}
-        <p>
-          这是一条动态数据：{this.state.text} : {this.state.number}
-        </p>
-        <button onClick={this.updateText.bind(this)}>add</button>
-        {/* 条件判断 */}
-        {this.state.number > 5 ? (
-          <p>
-            number: {this.state.number}, {">"} 5
-          </p>
-        ) : (
-          <p>
-            number: {this.state.number}, {"<="} 5
-          </p>
-        )}
-        {this.showNumber()}
-        {/* 列表循环 */}
-        <ul>
-          {this.state.numArr.map((item) => (
-            <li key={item}>{item}</li>
-          ))}
-        </ul>
-        {/* form 表单 */}
-        <input
-          type="text"
-          value={this.state.inputValue}
-          onChange={this.bindInputValue.bind(this)}
-        />{" "}
-        --{">"} {this.state.inputValue}
-        <br />
-        <textarea
-          value={this.state.textareaValue}
-          onChange={this.bindTextareaValue.bind(this)}
-        ></textarea>{" "}
-        --{">"} {this.state.textareaValue}
-      </div>
-    );
-  }
-
-  showNumber() {
-    if (this.state.number > 10) {
-      return (
-        <p>
-          number: {this.state.number}, {">"} 10
-        </p>
-      );
-    } else {
-      return (
-        <p>
-          number: {this.state.number}, {"<="} 10
-        </p>
-      );
-    }
-  }
-
-  bindInputValue(event) {
-    this.setState({
-      inputValue: event.target.value
-    });
-  }
-
-  bindTextareaValue(event) {
-    this.setState({
-      textareaValue: event.target.value
-    });
+    const num = 2;
+    return <div className="App">
+      <Router>
+        <nav>
+          <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to={`/jsx/${num}`}>Jsx</Link></li>
+            <li><Link to={`/home`}>"/home" redirect to "/"</Link></li>
+          </ul>
+        </nav>
+        <Switch>
+          <Route path="/" exact render={() => <h1>Welcome!</h1>} />
+          <Route path="/jsx/:num" component={JsxTest} />
+          {/* Redirect */}
+          <Redirect from="/home" to="/" />
+          {/*404 page*/}
+          <Route render={() => <h1>404</h1>} />
+        </Switch>
+      </Router>
+    </div>;
   }
 }
 
